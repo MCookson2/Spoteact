de0fadf014f64d3aaddb52ee82a6698be2aee95a
MCookson2 <matt.cookson2@gmail.com>
2021-06-26 20:16:26 +0100

    create SearchResults component function and pass down state via props testing

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,15 +1,24 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import SearchBar from "../SearchBar/SearchBar.js";
+import SearchResults from "../SearchResults/SearchResults.js";
 
 function App() {
-
+    const [searchResults, setSearchResults] = useState([
+        {name: "Song 1", artist: "Artist 1", album: "Album 1", id: 1},
+        {name: "Song 2", artist: "Artist 2", album: "Album 2", id: 2},
+        {name: "Song 3", artist: "Artist 3", album: "Album 3", id: 3}
+    ]);
+    
     return (
         <div className="App">
-        <header className="App-header">
-            <h1>Spoteact</h1>
-        </header>
-        <SearchBar />
+            <header className="App-header">
+                <h1>Spoteact</h1>
+            </header>
+            <SearchBar />
+            <div>
+                <SearchResults results={searchResults}/>
+            </div>
         </div>
     );
 }
--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -3,6 +3,21 @@ import './SearchResults.css';
 
 function SearchResults(props) {
 
+    const listTracks = () => {
+        const tracks = props.results;
+        console.log(tracks)
+        return tracks.map(track => (
+            <li key={track.id}>{track.name}, {track.artist}, {track.album}</li>
+        ))
+    }
+
+    return (
+        <div className="SearchResults">
+            <ul>
+                {listTracks()}
+            </ul>
+        </div>
+    )
 }
 
 export default SearchResults;
